43b3f543d714f8a7f21d1175c50b64b60e6b887f
Jonatan Suntaxi <josuntax@pichincha.com>
2022-06-14 12:24:47 -0500

    web api net

---
+++ GIT_SuntaxiJonatan/WebAPI/WebAPI/Models/Cuenta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Cuenta
+    {
+        public int IdCuenta { get; set; }
+        public string DocumentoIdentidad { get; set; }
+        public string Nombres { get; set; }
+        public string Saldo { get; set; }
+        public DateTime FechaRegistro { get; set; }
+    }
+}
